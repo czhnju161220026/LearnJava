727b97d7a62fc6de63b0be97dac6088f0c64a727
czhnju161220026 <997520420@qq.com>
2018-12-18 23:46:44 +0800

    update

---
+++ AnonymousInnerClassTest.java
@@ -0,0 +1,34 @@
+@FunctionalInterface
+interface Filter {
+    String filter(String string);
+}
+
+class FilterProvider {
+    public Filter provide1() {
+        return new Filter() {
+            @Override
+            public String filter(String string) {
+                return string.toLowerCase();
+            }
+        };
+    }
+
+    public Filter provide2() {
+        class UpperFilter implements Filter{
+            public String filter(String string) {
+                return string.toUpperCase();
+            }
+        }
+        return new UpperFilter();
+    }
+
+}
+
+public class AnonymousInnerClassTest {
+    public static void main(String[] args) {
+        FilterProvider fp = new FilterProvider();
+        Filter filter1 = fp.provide1();
+        System.out.println(filter1.filter("AvadaFwad"));
+        System.out.println(fp.provide2().filter("AvadaFwad"));
+    }
+}
